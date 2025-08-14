367d6d65901092f48c2e959d7bb7cdbb344a55aa
Alejo <alejo.lcc@gmail.com>
2025-08-14 13:40:08 -0300

    fix burguer button on smartphones

--- assets/js/hooks/MobileMenu.js
+++ assets/js/hooks/MobileMenu.js
@@ -5,8 +5,15 @@ let MobileMenu = {
     let menu = this.el.querySelector('[data-menu]');
 
     if (menuButton && menu) {
-      menuButton.addEventListener('click', () => {
+      menuButton.addEventListener('click', (event) => {
+        event.stopPropagation();
         menu.classList.toggle('hidden');
+      });
+
+      window.addEventListener('click', (event) => {
+        if (!menu.contains(event.target)) {
+          menu.classList.add('hidden');
+        }
       });
     }
   }
