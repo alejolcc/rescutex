bee21f66852f5bf6a874647fa36c39af8c434c46
Alejo <alejo.lcc@gmail.com>
2026-04-02 18:23:10 -0300

    fix Oban

---
+++ priv/repo/migrations/20260402212126_update_oban_schema.exs
@@ -0,0 +1,11 @@
+defmodule Rescutex.Repo.Migrations.UpdateObanSchema do
+  use Ecto.Migration
+
+  def up do
+    Oban.Migration.up(version: 12)
+  end
+
+  def down do
+    Oban.Migration.down(version: 11)
+  end
+end
